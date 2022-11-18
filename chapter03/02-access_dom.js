e487a5e93c97f894671fc9c41e4d16dbee79cd60
Vivek Kurhe <vivek.kurhe@launchventures.co>
2022-11-18 14:08:22 +0530

    dom manipuation chapter 03

---
+++ chapter03/02-access_dom.js
@@ -0,0 +1,19 @@
+/**
+ * Accessing the DOM
+ * 
+ * How do we use JavaSript to access the DOM?
+ * 
+ * There are many ways to access elements on our page
+ * document.getElementById() - returns an element object
+ * document.getElementsByName() - returns an HTML collection
+ * document.getElementsByClassName() - returns an HTML collection
+ * document.getElementsByTagName() - returns an HTML collection
+ * document.querySelector() - returns an element object
+ * document.querySelectorAll() - returns a NodeList
+ * 
+ * What is DOM?
+ * - The DOM is the browsers visual representation of your page
+ * - The browser gives us access to this structure via the DOM API
+ * - The DOM API gives us many methods we can use to access the DOM
+ * - We typically use JavaScript to execute these methods
+ */
