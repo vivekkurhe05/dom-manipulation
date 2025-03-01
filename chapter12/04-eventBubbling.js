1c493e56a102083e5f4d5885a2cb5d46cc029728
Vivek <vvekkurhe555@gmail.com>
2025-03-01 08:35:22 +0530

    chapter 12

---
+++ chapter12/04-eventBubbling.js
@@ -0,0 +1,5 @@
+/**
+ * Event Bubbling is the flow of each event through the DOM
+ * Phase1: the event starts at the root, and works its way to the target.
+ * Phase2: the event starts at the target, and bubbles its way back to the root.
+ */
