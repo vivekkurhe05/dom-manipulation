152e53148ce505564993c35d283026101d1b91ec
Vivek <vvekkurhe555@gmail.com>
2025-03-08 07:24:27 +0530

    chapter 13 and table2.html

---
+++ chapter13/05-classList-property.js
@@ -0,0 +1,19 @@
+/**
+ * The classList API returns a live DOMTokenList.
+ * DOMTokenList returns a list. This is a special lind of list that gives us all the
+ * methods mentioned below.
+ * 
+ * It gives us an easy way to grab all the classes of an element
+ * 
+ * This property is useful to add, remove and toggle CSS classes on an element
+ * 
+ * It was introduced by the HTML5 spec
+ * add()
+ * remove()
+ * contains()
+ * item()
+ * toString()
+ * toggle()
+ * value
+ * length
+ */
---
+++ chapter13/event-delegation.js
@@ -0,0 +1,4 @@
+/**
+ * Event delegation example
+ * Refer index.html
+ */
